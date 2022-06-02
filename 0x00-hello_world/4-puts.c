cce7cec9c8a641ac07dafe3694e1f224688ecd3c
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-02 14:52:49 +0200

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-int main(void) {
-puts("Programming is like building a multilingual puzzle");
+/*
+    Main function 4. Hello, puts 
+*/
+int main(void)
+{
+puts ("Programming is like building a multilingual puzzle");
 return(0);
 }
