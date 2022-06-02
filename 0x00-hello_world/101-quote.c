1d482c86a0f0d8efffe424824b3b8b0bdf97c718
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-02 16:53:00 +0200

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
