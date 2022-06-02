6c34fa67ea81d21f23cb0ad1a492ffec09adacef
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-02 15:20:32 +0200

    6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *  Printf: with proper grammar, but the outcome is a piece of art,
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char %s", sizeof(char));
+return (0);
+}
