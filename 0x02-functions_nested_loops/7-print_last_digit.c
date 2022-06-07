c29d27cc5043c04d267b766608b79779caf17322
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 17:08:12 +0200

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,15 @@
 int print_last_digit(int n)
 {
 	int x;
+
 	if (n < 0)
 	{
 		x = (n % 10) * -1;
-		_putchar(x);
 	}
 	else
 	{
 		x = (n % 10);
-		_putchar(x);
 	}
+	_putchar(x + '0');
 	return x;
 }
