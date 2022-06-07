d3f071809321b560c98a8a6e5f4c1a5730defbe3
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 16:56:59 +0200

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - the last digit of a number
+ *  - input
+ * Return: int
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,6 @@ void _putchar(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
 
 #endif /* MAIN_H */
