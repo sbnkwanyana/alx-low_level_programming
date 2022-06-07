ecdf8c7db7c11bf0ab01a5fac2840b08b8f7e6c3
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 16:48:39 +0200

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - returns 1 if positive and 0 if negative and -1 if 0
+ *  - input
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	return abs(n);
+}
