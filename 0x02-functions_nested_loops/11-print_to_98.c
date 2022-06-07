485d3fa5b3759c3255e4ba7389ca0bf1b580a75d
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 18:28:01 +0200

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,41 @@
 
 void print_to_98(int n)
 {
-	for (n = n ; n < 99; n++)
+	if(n > 98)
 	{
-		if (n < -9 || n > 9)
+		for (n = n ; n > 97; n--)
 		{
 			_putchar('0' + n / 10);
 			_putchar('0' + n % 10);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		else
+	}
+	else
+	{
+		for (n = n ; n < 99; n++)
 		{
-			_putchar('0' + n);
-		}
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
+			if (n < -9 || n > 9)
+			{
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+			}
+			else if (n < -99)
+			{
+
+			}
+			else
+			{
+				_putchar('0' + n);
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	_putchar('\n');
