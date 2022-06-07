f832af3c64258fd33473b14528b4771b269b861a
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 14:50:10 +0200

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_sign - uses islower function from ctype header
+ *  - input
+ * Return: int
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (43);
+	}
+	else
+	{
+		return (45);
+	}
+}
