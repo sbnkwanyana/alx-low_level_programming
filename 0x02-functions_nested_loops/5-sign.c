63d50cd3ddf16062236276d8578e4984f278e906
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 15:07:41 +0200

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar(43);
+		
 		return (43);
+	}
+	else if (n < 0)
+	{
+		putchar(45);
+		
+		return (45);
 	}
 	else
 	{
-		putchar(45);
-		return (45);
+		putchar('0');
+		return (0);
 	}
 }
