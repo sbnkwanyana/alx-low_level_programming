bc3767a7be23e2aa632b865efe905f166bc4e15f
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-07 17:33:42 +0200

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int times, by, equals;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (by = 0; by < 10; by++)
+		{
+			equals = times * by;
+			_putchar('0' + equals);
+			if (by != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
