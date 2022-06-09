2d25a7d9862ddea2cb999fd63834ebf74ca30049
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-09 10:55:23 +0200

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 - 14 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			_putchar('0' + i);
+			if (x > 9)
+			{
+				_putchar('0' + x % 2);
+			}
+		}
+		_putchar('\n');
+	}
+}
