0f5a5eda8056e6503144d35ad34b3f497a1b4b7c
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-09 21:45:01 +0200

    fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,22 @@ void print_square(int n)
 {
 	int i = 0, x = 0;
 
-	while (i < n)
+	if(n <= 0)
 	{
-		while (x < n)
-		{
-			_putchar(35);
-			x++;
-		}
-		i++;
-		x = 0;
 		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			while (x < n)
+			{
+				_putchar(35);
+				x++;
+			}
+			i++;
+			x = 0;
+			_putchar('\n');
+		}
 	}
 }
