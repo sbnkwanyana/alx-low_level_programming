e0d943d6dd1f22ce58fdd7afe9b776931d65eae1
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-09 21:56:20 +0200

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,30 @@ int main(void)
 {
 	int i;
 	
-	for (i = 0; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
+			if (i != 100)
+			{
+				printf(" ");
+			}
 		}
 	}
 
