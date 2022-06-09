0852ad9ad0b7e61a1beb9a3f0bf15304af3982b0
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-09 20:57:35 +0200

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,13 @@ void print_line(int n)
 {
 	int i;
 
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n ; n++)
+		for (i = 0; i < n ; i++)
 		{
 			_putchar('\\');
 			_putchar('\n');
