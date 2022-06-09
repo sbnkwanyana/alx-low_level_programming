ab78b56359f656392e4fde5f2d12f317de6f4dd9
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-09 11:00:41 +0200

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print straight line on terminal
+ * @n: number of lines
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n ; n++)
+	{
+		_putchar('_');
+	}
+}
