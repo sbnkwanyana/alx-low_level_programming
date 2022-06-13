8a477bf4bb863e8e97067e9d8fb474d7afbab224
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-13 15:38:53 +0200

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rev_string - reverse string
+ * @s: string value
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+    int i, len, temp;
+
+    len = strlen(s);
+      
+    for (i = 0; i < len/2; i++)
+    {  
+        temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+    }
+}
