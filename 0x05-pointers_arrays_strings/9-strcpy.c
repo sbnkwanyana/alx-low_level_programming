c4bc5d134ba1f699fa0b2850341fd604e39d555e
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-13 18:06:57 +0200

    9-strcopy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char
+ * @src: char
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy (dest, src));
+}
