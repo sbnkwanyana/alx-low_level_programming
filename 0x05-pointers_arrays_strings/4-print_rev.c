09a584f8d932d63ac1d6b95681f58d3b108bf190
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-13 14:17:55 +0200

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,14 @@
 
 void print_rev(char *s)
 {
-	int i, len, temp;  
+	int i, len, temp;
+
 	len = strlen(s);
-	for (i = 0; i < len/2; i++)  
-	{  
-		temp = s[i];  
-		s[i] = s[len - i - 1];  
-		s[len - i - 1] = temp;  
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
