9dd16037fe81758c112b9c3672895a662248dc54
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-13 17:01:44 +0200

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@
 
 void rev_string(char *s)
 {
-    int i, temp;
+	int i, temp;
 
-    for (i = 0; i < strlen(s) / 2; i++)
-    {  
-        temp = s[i];
-        s[i] = s[strlen(s) - i - 1];
-        s[strlen(s) - i - 1] = temp;
-    }
-    printf("%s", s);
+	for (i = 0; i < (strlen(s) / 2); i++)
+	{
+		temp = s[i];
+		s[i] = s[strlen(s) - i - 1];
+		s[strlen(s) - i - 1] = temp;
+	}
+	printf("%s", s);
 }
