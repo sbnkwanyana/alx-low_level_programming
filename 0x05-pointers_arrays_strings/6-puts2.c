3f14457cbe903a239b7e8dd78fc393b8d939308c
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-13 17:14:50 +0200

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: string parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+}
