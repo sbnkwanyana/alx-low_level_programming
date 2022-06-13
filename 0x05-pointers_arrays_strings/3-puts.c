3302a0cf6fd8353bda0b5122df4d982e6428392f
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-13 12:56:20 +0200

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints string to stdout ending in new line
+ * @str: string parameter
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(*str);
+	puts("\n");
+}
