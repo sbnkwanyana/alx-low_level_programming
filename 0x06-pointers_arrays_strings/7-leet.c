0deee9944e047ae70d0ff6c2208e473fe34acdcd
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-15 16:47:43 +0200

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * leet - function encodes a string into 1337
+ * @str: string to encode
+ * Return: char pointer
+ */
+
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] == 4;
+		}
+		if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] == 3;
+		}
+		if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] == 0;
+		}
+		if (str[i] == 'at' || str[i] == 'T')
+		{
+			str[i] == 7;
+		}
+		if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] == 1;
+		}
+	}
+}
