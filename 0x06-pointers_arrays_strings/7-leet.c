179b5fa89529d7b6ae16ea0ba6aeec7d46738a76
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-16 11:21:37 +0200

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,23 +15,23 @@ char *leet(char *str)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
 		{
-			str[i] = 4;
+			str[i] = '4';
 		}
 		if (str[i] == 'e' || str[i] == 'E')
 		{
-			str[i] = 3;
+			str[i] = '3';
 		}
 		if (str[i] == 'o' || str[i] == 'O')
 		{
-			str[i] = 0;
+			str[i] = '0';
 		}
 		if (str[i] == 't' || str[i] == 'T')
 		{
-			str[i] = 7;
+			str[i] = '7';
 		}
 		if (str[i] == 'l' || str[i] == 'L')
 		{
-			str[i] = 1;
+			str[i] = '1';
 		}
 	}
 	return (str);
