e624ef54e5f5c3fb4f4e2d0554e981462e03e9d0
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-20 13:13:25 +0200

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset - function that fills memory with a constant byte
- * @s: 
- * @b:
- * @n:
+ * @s: memory pointer to fil
+ * @b: byte value to be filled
+ * @n: number of bytes to fill
  * Return: pointer to the memory area s
  */
 
