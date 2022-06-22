de090766f0c863bbde592b1e552eaf22eaa94994
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-22 13:05:15 +0200

    _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the power of a given number recursively
+ * @x: base number
+ * @y: eponent value
+ * Return: int power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
