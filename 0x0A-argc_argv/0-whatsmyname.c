9055a71095e0dfa0575e0bcd1ae95b4c135765c0
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-24 11:37:29 +0200

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry that prints out the name if the application
+ * @argc: unused parameter
+ * @argv: array argument list
+ * Return - 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry that prints out the number of arguments
+ * @argc: argument count including app name
+ * @argv: unused arguments array
+ * Return - 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc);
+	return (0);
+}
