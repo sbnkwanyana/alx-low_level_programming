04ef91fb831b953f4808b579e7c04bf2847ea5e3
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-06-28 07:34:51 +0200

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 
-	mem = malloc(sizeof(str));
+	mem = malloc(strlen(str));
 
 	if (mem == NULL)
 	{
@@ -32,5 +32,4 @@ char *_strdup(char *str)
 	}
 
 	return (mem);
-
 }
