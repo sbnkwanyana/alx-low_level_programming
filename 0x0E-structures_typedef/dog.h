6bddfe315663cd896ad7b660b89b0ff72bb63047
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-07-04 18:05:48 +0200

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+
+#define DOG
+
+/**
+ * struct dog - represents dog structure
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
