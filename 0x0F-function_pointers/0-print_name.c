db56ba88d276b7f9e8c624d8ad082b260e50446f
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-07-06 19:48:00 +0200

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,7 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
- *  print_name - function that prints a name
+ * print_name - function that prints a name
  * @name: name to print
  * @f: function pointer
  * Return: void
