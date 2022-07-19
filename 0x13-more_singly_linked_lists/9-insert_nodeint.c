d1858534577ac52fb0928da348f3e8489d2a971f
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-07-19 13:49:55 +0200

    code update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,8 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	do {
 		if (current == NULL)
 		{
+			node->next = *head;
 			*head = node;
-			return (*head);
+			return (node);
 		}
 		if (current->next != NULL)
 		{
